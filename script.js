5320a6cf76455a989bea1370307b806d89bafbc2
Vedant-01 <vedantambigar1@gmail.com>
2026-02-25 06:23:26 +0530

    script.js

--- script.js
+++ script.js
@@ -17,31 +17,3 @@ document.addEventListener("keydown", function (e) {
     case "4": location.hash = "contact"; break;
   }
 });
-
-
-fetch("portfolio.json")
-  .then(res => res.json())
-  .then(data => {
-    document.querySelector("h1").textContent = data.name;
-  });
-// Contact form handling
-const form = document.forms["f1"];
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); // stop page reload
-
-  const name = form.t1.value.trim();
-  const email = form.t2.value.trim();
-  const message = form.t3.value.trim();
-
-  if (name === "" || email === "" || message === "") {
-    alert("Please fill all fields");
-    return;
-  }
-
-  alert("Thank you " + name + "! Your message has been sent .");
-
-  // clear form
-  form.reset();
-});
-
